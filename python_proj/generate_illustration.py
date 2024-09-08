0fdf8be3878143feb6e96bd4b7be0c83a402cbf4
jongwoo <izowooi@hotmail.com>
2024-09-08 13:13:41 +0900

    여러 이미지를 생성가능하도록 예시 코드 변경

--- python_proj/generate_illustration.py
+++ python_proj/generate_illustration.py
@@ -1,4 +1,6 @@
-from midjourney_sdk_py import Midjourney
+import time
+
+from midjourney_sdk_py_multiple import Midjourney
 import os
 from dotenv import load_dotenv
 
@@ -9,12 +11,19 @@ discord_user_token = os.getenv('DISCORD_USER_TOKEN')
 
 midjourney = Midjourney(discord_channel_id, discord_user_token)
 
-prompt = "An illustration showing the seven colors of the rainbow, labeled: red, orange, yellow, green, blue, indigo, violet. a cute 3d style"
+prompt = "A bright dream world with a child flying with balloons, meeting friendly animals, and playing with toys.." +\
+         " Pixar style, 3D rendered, bright colors, expressive emotions, smooth textures, detailed lighting, cinematic composition"
 options = {
+    #"ar": "16:9",
     "ar": "1:1",
     "v": "6.1",
 }
 
-message = midjourney.generate(prompt, options)
+message = midjourney.generate(prompt, options, upscale_index=0)
 
-print(message['upscaled_photo_url'])
+print(f"index 0 : {message['upscaled_photo_url']}")
+
+if 'imagine' in message:
+    imagine = message['imagine']
+    upscaled_photo_url = midjourney.upscale(imagine, 3)
+    print(f'index 1 : {upscaled_photo_url}')
