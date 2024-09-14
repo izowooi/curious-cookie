495e499495d06548dd5c8923cbc24e2ed0261389
jongwoo <izowooi@hotmail.com>
2024-09-14 23:40:57 +0900

    설정 UI 통일되게 수정

--- flutter_proj/lib/widget/setting_widget.dart
+++ flutter_proj/lib/widget/setting_widget.dart
@@ -7,10 +7,12 @@ class SettingWidget extends StatefulWidget {
 
 class _SettingWidgetState extends State<SettingWidget> {
   String _selectedStyle = '2d';
+  String _selectedLanguage = 'ko';
   double _fontSize = 32.0;
 
   @override
   Widget build(BuildContext context) {
+    var textStyle = TextStyle(fontSize: 18);
     return Scaffold(
       appBar: AppBar(
         title: Text('설정'),
@@ -20,75 +22,93 @@ class _SettingWidgetState extends State<SettingWidget> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            Divider(
+              color: Colors.grey, // 선 색상
+              thickness: 1, // 선 두께
+              indent: 0, // 왼쪽 여백
+              endIndent: 0, // 오른쪽 여백
+            ),
             // 화풍 선택
+            Text('화풍 선택', style: textStyle),
             Row(
               children: [
-                Text('화풍 선택', style: TextStyle(fontSize: 18)),
-                Expanded(
-                  child: RadioListTile<String>(
-                    title: Text('2D'),
-                    value: '2d',
-                    groupValue: _selectedStyle,
-                    onChanged: (value) {
-                      setState(() {
-                        _selectedStyle = value!;
-                      });
-                    },
-                  ),
-                ),
-                Expanded(
-                  child: RadioListTile<String>(
-                    title: Text('3D'),
-                    value: '3d',
-                    groupValue: _selectedStyle,
-                    onChanged: (value) {
-                      setState(() {
-                        _selectedStyle = value!;
-                      });
-                    },
-                  ),
-                ),
-                Expanded(
-                  child: RadioListTile<String>(
-                    title: Text('엄마'),
-                    value: '엄마',
-                    groupValue: _selectedStyle,
-                    onChanged: (value) {
-                      setState(() {
-                        _selectedStyle = value!;
-                      });
-                    },
-                  ),
-                ),
+                
+                ...['2d', '3d', '엄마'].map((style) {
+                  return Expanded(
+                    child: RadioListTile<String>(
+                      title: Text(style.toUpperCase()),
+                      value: style,
+                      groupValue: _selectedStyle,
+                      onChanged: (value) {
+                        setState(() {
+                          _selectedStyle = value!;
+                        });
+                      },
+                    ),
+                  );
+                }).toList(),
               ],
             ),
-            SizedBox(height: 16),
+            SizedBox(height: 8),
+            Text('언어 선택', style: textStyle),
             Row(
               children: [
-                Text('폰트 크기', style: TextStyle(fontSize: 18)),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 38),
-                      child: Slider(
-                        value: _fontSize,
-                        min: 32,
-                        max: 128,
-                        divisions: 20,
-                        label: _fontSize.round().toString(),
-                        onChanged: (value) {
-                          setState(() {
-                            _fontSize = value;
-                          });
-                        },
-                      ),
+                ...['kr', 'en', 'ar'].map((style) {
+                  return Expanded(
+                    child: RadioListTile<String>(
+                      title: Text(style.toUpperCase(),),
+                      value: style,
+                      groupValue: _selectedLanguage,
+                      onChanged: (value) {
+                        setState(() {
+                          _selectedLanguage = value!;
+                        });
+                      },
+                    ),
+                  );
+                }).toList(),
+              ],
+            ),
+            Row(
+              children: [
+                ...['jp', 'cn'].map((style) {
+                  return Expanded(
+                    child: RadioListTile<String>(
+                      title: Text(style.toUpperCase(),),
+                      value: style,
+                      groupValue: _selectedLanguage,
+                      onChanged: (value) {
+                        setState(() {
+                          _selectedLanguage = value!;
+                        });
+                      },
+                    ),
+                  );
+                }).toList(),
+              ],
+            ),
+            SizedBox(height: 8),
+            Text('폰트 크기 (${_fontSize.round().toString()})', style: TextStyle(fontSize: 18)),
+            Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 0),
+                    child: Slider(
+                      value: _fontSize,
+                      min: 32,
+                      max: 128,
+                      label: _fontSize.round().toString(),
+                      onChanged: (value) {
+                        setState(() {
+                          _fontSize = value;
+                        });
+                      },
                     ),
                   ),
-                  Text(_fontSize.round().toString(), style: const TextStyle(fontSize: 18)
                 ),
               ],
             ),            
-            SizedBox(height: 16),
-
           ],
         ),
       ),
