13bd628017e77952cdad146dd2a2eca144880e5b
jongwoo <izowooi@hotmail.com>
2024-09-14 23:53:54 +0900

    대본 만든 후에 일러스트 까지 만들도록 main 파일 수정

--- python_proj/main.py
+++ python_proj/main.py
@@ -27,20 +27,20 @@ def main():
     for question_id, question_text in question_dict.items():
         print(f'question_id: {question_id}, question_text: {question_text}')
         script_dict, prompt_list, category = openai_chat_bot.generate_script(question_text)
-        script_id_list = fb_manager.append_script_list_to_db(question_id, script_dict, prompt_list)
+        fb_manager.append_script_list_to_db(question_id, script_dict, prompt_list)
         fb_manager.set_generated_script(question_id)
 
         gen_script_time = time.time()
-        #todo:script_id_list 로직 추가 후 테스트 안 되었음.
-        midjourney_bot.generate_and_save_illustrations_list(script_id_list, prompt_list)
-        gen_illustration_time = time.time()
-        #todo:일러스트 만든 후에 디비에 저장하는 코드 추가
+    gen_illustration()
+    #midjourney_bot.generate_and_save_illustrations_list(script_id_list, prompt_list)
+    gen_illustration_time = time.time()
+    #todo:일러스트 만든 후에 디비에 저장하는 코드 추가
 
-        print(f'gen_script_time: {gen_script_time - start_time}')
-        print(f'gen_illustration_time: {gen_illustration_time - gen_script_time}')
-        # 6개의 이미지 ( 4개는 업스케일 ) 80초 정도 소요됨, 한 컷을 60초로 가정하면 10컷은 10분.
-        # 질문 하나에 10분을 가정하면 1시간에 6개의 질문을 처리할 수 있음.
-        # 미드저니 요금제가 200분을 주면 20개 정도 한달에 만들 수 있음. 몇개 못 만드네.
+    print(f'gen_script_time: {gen_script_time - start_time}')
+    print(f'gen_illustration_time: {gen_illustration_time - gen_script_time}')
+    # 6개의 이미지 ( 4개는 업스케일 ) 80초 정도 소요됨, 한 컷을 60초로 가정하면 10컷은 10분.
+    # 질문 하나에 10분을 가정하면 1시간에 6개의 질문을 처리할 수 있음.
+    # 미드저니 요금제가 200분을 주면 20개 정도 한달에 만들 수 있음. 몇개 못 만드네.
 
 
 def test_gen_script():
@@ -51,7 +51,7 @@ def test_gen_script():
         fb_manager.set_processed_question(question_id)
 
 
-def test_gen_illustration():
+def gen_illustration():
     no_illustration_dict = fb_manager.get_no_illustration_from_db()
 
     for script_id, prompt in no_illustration_dict.items():
@@ -60,6 +60,6 @@ def test_gen_illustration():
         fb_manager.set_processed_script(script_id)
 
 
-#main()
-test_gen_illustration()
+main()
+#gen_illustration()
 #test_gen_script()
